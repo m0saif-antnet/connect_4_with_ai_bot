cd8613ef14430134016ec5bccf730f54922d6f86
Marwan3laa <marwan2all2021@gmail.com>
2026-05-08 16:26:41 +0300

    minimax.py , No change in the code itself
    minor changes in the comments making it more clean

--- ai/minimax.py
+++ ai/minimax.py
@@ -1,13 +1,9 @@
-
 import math
-
 from .heuristics import score_position
-
 from game.board import (
     copy_board,
     apply_move,
     get_valid_moves)
-
 from game.rules import(
     winning_move,
     is_terminal_state,
@@ -24,23 +20,18 @@ def evaluate_terminal_state(board: list[list[int]], ai_piece: int, opponent_piec
         return -1_000      
     else:
         return 0              
-
-
+        
 def simulate_move(board: list[list[int]], col: int, piece: int) -> list[list[int]]:
     new_board = copy_board(board)
     apply_move(new_board, col, piece)
     return new_board
-
-
-
+    
 def get_ordered_moves(valid_moves: list[int]) -> list[int]:
     center = 3  
     return sorted(valid_moves, key=lambda col: abs(center - col))  # Sort by distance from center (closest to center = tried first)
 
 
-#  ============================================================================= VERSION 1: Basic Minimax () =============================================================================
-
-
+#  ============================================================================= VERSION 1: Basic Minimax () ============================================================================= #
 def minimax(
     board: list[list[int]],
     depth: int,
@@ -55,15 +46,12 @@ def minimax(
     # --- Base cases: stop recursion ---
 
     if terminal:
-       
         return (None, evaluate_terminal_state(board, ai_piece, opponent_piece))
-
-    if depth == 0:
         
+    if depth == 0:
         return (None, score_position(board, ai_piece, opponent_piece))
-    
+        
     if not valid_moves:
-       
         return (None, 0)
 
     # --- Recursive cases ---
@@ -97,7 +85,7 @@ def minimax(
 
         return (best_col, best_score)
 
-# ============================================================================= Minimax WITH Alpha-Beta Pruning =============================================================================
+# ============================================================================= Minimax WITH Alpha-Beta Pruning ============================================================================= #
 
 def minimax_alpha_beta(
     board: list[list[int]],
@@ -141,14 +129,12 @@ def minimax_alpha_beta(
             if score > best_score:
                 best_score = score
                 best_col = col
-
             alpha = max(alpha, best_score)
 
             # Prune: the opponent already has a path that's better for them
             # than anything we could get here — stop searching this branch
             if alpha >= beta:
                 break   # Beta cutoff
-
         return (best_col, best_score)
 
     else:
